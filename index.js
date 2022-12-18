84c6d7b6aaa5e2dfe696a5f88588fb3212231e45
kekelberi <alexei.gaponov@gmail.com>
2022-12-18 13:25:58 -0600

    save

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
-const http = require("http")
+const fs = require("fs");
+const http = require("http");
 
-const server = http.createServer((req, res) => {
-  res.write("bd")
-  res.end()
-})
-server.listen(3000)
+const delay = (ms) => {
+  return new Promise((rev, rej) => {
+    setTimeout(() => {
+      rev();
+    }, ms);
+  });
+};
+
+const readFile = (path) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, async (e, data) => {
+      if (e) reject(e);
+      else resolve(data);
+    });
+  });
+};
+
+const server = http.createServer(async (req, res) => {
+  switch (req.url) {
+    case "/":
+      try {
+        // await delay(3000);
+        const data = await readFile("index.html");
+        res.write(data);
+        res.end();
+      } catch (err) {
+        res.write("err = ");
+      }
+      break;
+    default:
+      res.write("404");
+      res.end();
+  }
+});
+server.listen(3000);
